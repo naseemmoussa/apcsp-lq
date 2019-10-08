f6cf24e8e2494bc8f05f4ff548387d157d45bd1c
Naseem Moussa <naseemmoussa7@gmail.comgit config --global user.email naseemmoussa7@gmail.com>
2019-10-08 09:49:02 -0600

    biggertwo finished

--- listquiz.rb
+++ listquiz.rb
@@ -12,3 +12,27 @@ end
 puts threeeven([2,1,3,5])
 puts threeeven([2,4,12,5])
 
+def biggertwo(list1,list2)
+    sum1 = 0
+    sum2 = 0
+    list1.each do |num|
+        sum1 = sum1 + num
+    end
+    list2.each do |num|
+        sum2 = sum2 + num
+    end
+    if sum1 < sum2
+        return list2
+    else
+        return list1
+    end
+end
+
+print biggertwo([1,2],[3,4])
+print biggertwo([1,7],[4,4])
+
+
+
+
+
+
